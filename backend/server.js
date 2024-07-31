dbb80dd0d73529765d229eb76fdf8af8283f869a
jirapan <jeraphan2000@gmail.com>
2024-08-01 04:45:16 +0700

    1- auth controllers added

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 
 import authRoutes from './routes/auth.routes.js';
 import connectMongoDB from './db/connectMongoDB.js';
@@ -10,6 +11,11 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 // console.log(process.env.MONGO_URI);
+
+app.use(express.json()); // to parse req.body
+app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
+
+app.use(cookieParser());
 
 app.use('/api/auth', authRoutes);
 
